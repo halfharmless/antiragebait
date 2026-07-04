ffd483dca02977a3e6623e234da6892bd7484402
halfharmless <halfharmless7me7@gmail.com>
2026-07-04 15:44:51 -0500

    Delete ChatFilterMod.java

--- ChatFilterMod.java
+++ ChatFilterMod.java
@@ -1,15 +1,1 @@
-package com.example.chatfilter;
-
-import net.fabricmc.api.ClientModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class ChatFilterMod implements ClientModInitializer {
-    public static final String MOD_ID = "chatfilter";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-    @Override
-    public void onInitializeClient() {
-        LOGGER.info("Chat Filter loaded - 'fuck' will be replaced with 'frick' in outgoing chat messages.");
-    }
-}
+//oops, file in src
